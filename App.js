dfc0741f14cf540a12edc2ba41d6b0b8632ef39d
Alienor <alienordms@gmail.com>
2023-10-22 12:15:03 +0200

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 export default function App() {
   return (
