4d95162c5b7fec22c5dfc3e07c6349875e00ef0a
Your name <Your email>
2023-10-23 17:31:37 +0200

    first commit

--- App.js
+++ App.js
@@ -1,26 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import LoadingPage from "./screens/loadingPage";
+import SignScreen from "./screens/sign";
+
+const Stack = createNativeStackNavigator();
+
+// const AppStack = () => {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name='Loading' component={LoadingPage} options={{ headerShown: false }} />
+//       {/* Add other screens here */}
+//     </Stack.Navigator>
+//   );
+// };
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Provider store={configureStore({})}>
+    /* <AppStack /> */
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Sign' component={SignScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // {/* </Provider> */}
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/sign.js
@@ -0,0 +1,241 @@
+import { StyleSheet, Text, TouchableOpacity, View, ImageBackground, Modal, TextInput } from "react-native";
+import * as Font from "expo-font";
+// import { GoogleSignin, GoogleSigninButton, statusCodes } from "@react-native-google-signin/google-signin";
+import { useState } from "react";
+
+export default function SignScreen({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalType, setModalType] = useState("");
+
+  const handleModalToggle = type => {
+    setModalType(type);
+    setModalVisible(!modalVisible);
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground style={styles.imagBgd} source={require("../assets/finalimageConnection.png")}>
+        <Text style={styles.title1}>Fable</Text>
+        <Text style={styles.title2}>Forge</Text>
+      </ImageBackground>
+      <Text style={styles.subTitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp.</Text>
+      <TouchableOpacity style={styles.btnHome} onPress={() => handleModalToggle("register")}>
+        <Text style={styles.textBtn}>S'inscrire gratuitement</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.btnGoogle} // </View>onPress={() => handleGoogleSignIn()}
+      >
+        <Text style={styles.textBtn1}>Se connecter avec Google</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btnFacebook}>
+        <Text style={styles.textBtn1}>Se connecter avec Facebook</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btnInput} onPress={() => handleModalToggle("connexion")}>
+        <Text style={styles.textBtn1}>Se connecter</Text>
+      </TouchableOpacity>
+      <Modal visible={modalVisible} animationType='slide' transparent={true}>
+        <View style={styles.mdlctn}>
+          <View style={styles.modalContainer}>
+            <TouchableOpacity onPress={() => handleModalToggle("")}>
+              <Text style={styles.btnCloseModal}>x</Text>
+              <Text style={styles.titleModal}> Fable Forge</Text>
+            </TouchableOpacity>
+            {modalType === "register" && (
+              <>
+                <TextInput style={styles.input} placeholder='Prénom' placeholderTextColor='white'></TextInput>
+                <TextInput style={styles.input} placeholder='Pseudonyme' placeholderTextColor='white'></TextInput>
+                <TextInput style={styles.input} placeholder='Email' placeholderTextColor='white'></TextInput>
+                <TextInput style={styles.input} placeholder='Mot de Passe' placeholderTextColor='white'></TextInput>
+                <TouchableOpacity style={styles.btnValidate}>
+                  <Text style={styles.textBtnValidate}>Valider</Text>
+                </TouchableOpacity>
+              </>
+            )}
+            {modalType === "connexion" && (
+              <>
+                <TextInput style={styles.input} placeholder='Pseudonyme ou Email' placeholderTextColor='white'></TextInput>
+                <TextInput style={styles.input} placeholder='Mot de Passe' placeholderTextColor='white'></TextInput>
+                <TouchableOpacity style={styles.btnValidate}>
+                  <Text style={styles.textBtnValidate}>Valider</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#2C1A51",
+  },
+  mdlctn: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0,0,0, 0.5)",
+  },
+  imagBgd: {
+    flex: 2,
+    width: "100%",
+    height: "100%",
+    // backgroundColor:
+  },
+  title1: {
+    fontFamily: "Lato",
+    fontSize: 28,
+    // margin: 170,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#FFFFFF",
+    marginTop: 350,
+  },
+  title2: {
+    // fontFamily: "Lato",
+    fontSize: 56,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#FFFFFF",
+    lineHeight: 60,
+  },
+  subTitle: {
+    width: "80%",
+    color: "#FFFFFF",
+    textAlign: "center",
+    fontSize: 14,
+    marginBottom: 40,
+  },
+  btnConnect: {
+    margin: 10,
+    width: "90%",
+  },
+  btnHome: {
+    backgroundColor: "#FFCE4A",
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    paddingLeft: 80,
+    paddingRight: 80,
+    width: "80%",
+  },
+  btnGoogle: {
+    backgroundColor: "transparent",
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "#FFFFFF",
+  },
+  btnFacebook: {
+    backgroundColor: "transparent",
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "#FFFFFF",
+  },
+  btnInput: {
+    backgroundColor: "transparent",
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "#FFFFFF",
+    marginBottom: 80,
+  },
+  textBtn: {
+    textAlign: "center",
+  },
+  textBtn1: {
+    textAlign: "center",
+    color: "#FFFFFF",
+  },
+  modalContainer: {
+    width: 350, // Adjust the width as per your requirement
+    height: 350, // Adjust the height as per your requirement
+    marginTop: 150,
+    marginLeft: 30,
+    backgroundColor: "#6B5F85",
+    borderRadius: 20, // Adjust the borderRadius as per your requirement
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    // backdropFilter: "blur(5px)",
+  },
+
+  //   modalRegister: {
+  //     width: 350, // Adjust the width as per your requirement
+  //     height: 350, // Adjust the height as per your requirement
+  //     marginTop: 150,
+  //     marginLeft: 30,
+  //     backgroundColor: "#6B5F85",
+  //     borderRadius: 20, // Adjust the borderRadius as per your requirement
+  //     padding: 20,
+  //     alignItems: "center",
+  //     justifyContent: "center",
+  //   },
+
+  //   modalConnexion: {
+  //     width: 350, // Adjust the width as per your requirement
+  //     height: 350, // Adjust the height as per your requirement
+  //     marginTop: 150,
+  //     marginLeft: 30,
+  //     backgroundColor: "#6B5F85",
+  //     borderRadius: 20, // Adjust the borderRadius as per your requirement
+  //     padding: 20,
+  //     alignItems: "center",
+  //     justifyContent: "center",
+  //   },
+
+  imagBgdModal: {
+    height: "100%",
+    width: "100%",
+    opacity: 0.5,
+  },
+
+  titleModal: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    padding: 10,
+    marginLeft: 50,
+  },
+  btnCloseModal: {
+    fontSize: 20,
+    padding: 10,
+    marginTop: 10,
+    marginLeft: 200,
+    color: "white",
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    borderColor: "white",
+    color: "white",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    borderRadius: 10,
+  },
+  btnValidate: {
+    backgroundColor: "#FFCE4A",
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    paddingLeft: 80,
+    paddingRight: 80,
+    width: "80%",
+    marginBottom: 20,
+  },
+  textBtnValidate: {
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
