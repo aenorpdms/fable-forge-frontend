4ae974c89df3618ed0e5ea46058ddfaea7fada34
Your name <Your email>
2023-10-24 09:34:06 +0200

    second commit

---
+++ screens/settings.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, TouchableOpacity, View, ImageBackground, Modal, TextInput } from "react-native";
+import * as Font from "expo-font";
+
+import { useState } from "react";
+
+export default function settingsScreen({ navigation }) {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({});
