650d2341bad2766bc231fa90bdbacd1c45f46139
Your name <Your email>
2023-10-25 16:34:56 +0200

    commit end of the day

--- screens/StoryGenerationStep2Screen.js
+++ screens/StoryGenerationStep2Screen.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import { View, Text, ImageBackground, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
 export default function StoryGenerationStep2Screen({ navigation }) {
+  const [buttonColor, setButtonColor] = useState("#2C1A51");
+
   // Story display page:
   // const handleStoryDisplay = () => {
   //   // navigate to Story display page
@@ -16,6 +19,41 @@ export default function StoryGenerationStep2Screen({ navigation }) {
         <Text style={styles.title2bis}>Etape 2/5</Text>
         <Text style={styles.title3}>Interactif</Text>
       </ImageBackground>
+      <View style={styles.containerStory}>
+        <ScrollView contentContainerStyle={styles.containerInformation} indicatorStyle='white'>
+          <Text style={styles.titleContainer}>Longueur</Text>
+          <TouchableOpacity style={styles.btnSizeStory} onPress={() => setButtonColor("#2C1A51")}>
+            <Text style={styles.sizeTextBtn}>Courte</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnSizeStoryOn}>
+            <Text style={styles.sizeTextBtnOn}>Moyenne</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnSizeStory}>
+            <Text style={styles.sizeTextBtn}>Longue</Text>
+          </TouchableOpacity>
+          <Text style={styles.titleContainer}>Type de fin</Text>
+          <View style={styles.leftContainer}>
+            <View style={styles.typeEndLeft}>
+              <ImageBackground style={styles.imagBgdAbo} source={require("../assets/ImageBibliotheque.png")}></ImageBackground>
+              <Text style={styles.textTypeEnd}> fin heureuse</Text>
+            </View>
+            <View style={styles.typeEndLeft}>
+              <ImageBackground style={styles.imagBgdAbo} source={require("../assets/ImageBibliotheque.png")}></ImageBackground>
+              <Text style={styles.textTypeEnd}>fin triste</Text>
+            </View>
+          </View>
+          <View style={styles.rightContainer}>
+            <View style={styles.typeEndRight}>
+              <ImageBackground style={styles.imagBgdAbo} source={require("../assets/ImageBibliotheque.png")}></ImageBackground>
+              <Text style={styles.textTypeEnd}>fin ouverte</Text>
+            </View>
+            <View style={styles.typeEndRight}>
+              <ImageBackground style={styles.imagBgdAbo} source={require("../assets/ImageBibliotheque.png")}></ImageBackground>
+              <Text style={styles.textTypeEnd}>fin morale</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -30,25 +68,125 @@ const styles = StyleSheet.create({
   imagBgd: {
     flex: 2,
     width: "100%",
-    height: "35%",
+    height: "75%",
     marginTop: "-12%",
   },
   title1: {
     color: "white",
     fontSize: 21,
-    top: "20%",
+    top: "50%",
+    marginLeft: 10,
   },
   title2: {
     color: "#FFCE4A",
     fontSize: 16,
-    top: "20%",
+    top: "50%",
+    marginLeft: 10,
   },
   title2bis: {
     color: "#FFCE4A",
-    top: "20%",
+    marginLeft: 300,
+    top: "50%",
   },
   title3: {
     color: "white",
-    top: "20%",
+    top: "50%",
+    marginLeft: 10,
+  },
+  containerInformation: {
+    height: 700,
+    borderWidth: 2,
+  },
+  containerStory: {
+    height: 600,
+    borderWidth: 2,
+
+    bottom: 30,
+    minHeight: "60%",
+    width: "90%",
+    borderRadius: 10,
+    backgroundColor: "#6B5F85",
+    padding: 20,
+  },
+  leftContainer: {
+    flexDirection: "row",
+    width: "50%",
+    // borderColor: "red",
+    // borderWidth: 1,
+    marginLeft: 10,
+    top: 30,
+    // margin: 10,
+  },
+  rightContainer: {
+    flexDirection: "row",
+    width: "50%",
+    bottom: 60,
+    // borderColor: "red",
+    // borderWidth: 1,
+    marginLeft: 10,
+    margin: 10,
+  },
+
+  titleContainer: {
+    color: "white",
+    fontSize: 18,
+    textAlign: "center",
+  },
+  btnSizeStory: {
+    borderColor: "white",
+    backgroundColor: "#2C1A51",
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "#FFCE4A",
+    borderRadius: 10,
+    padding: 5,
+  },
+  btnSizeStoryOn: {
+    backgroundColor: "#FFCE4A",
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 5,
+  },
+  sizeTextBtn: {
+    color: "white",
+    textAlign: "center",
+    padding: 10,
+  },
+  sizeTextBtnOn: {
+    color: "black",
+    textAlign: "center",
+    padding: 10,
+  },
+  typeEndLeft: {
+    flexDirection: "column",
+    width: "80%",
+    height: "40%",
+    bottom: 10,
+    // borderColor: "red",
+    // borderWidth: 1,
+    borderRadius: 15,
+    margin: 10,
+  },
+  typeEndRight: {
+    flexDirection: "column",
+    width: "80%",
+    height: "40%",
+    bottom: 40,
+    // borderColor: "red",
+    // borderWidth: 1,
+    borderRadius: 15,
+    margin: 10,
+  },
+  imagBgdAbo: {
+    width: "100%",
+    height: "100%",
+    overflow: "hidden",
+    borderRadius: 15,
+  },
+  textTypeEnd: {
+    textAlign: "center",
+    color: "white",
+    marginTop: 10,
   },
 });
