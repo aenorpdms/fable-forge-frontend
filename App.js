7e375f45dd17625b92ce4641929829e3af52fd6c
Alienor <alienordms@gmail.com>
2023-10-27 11:22:15 +0200

    do

--- App.js
+++ App.js
@@ -25,6 +25,10 @@ import {
 
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
+// redux-persist imports
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import SignScreen from "./screens/SignScreen";
 import HomeScreen from "./screens/HomeScreen";
--- screens/ProfilScreen.js
+++ screens/ProfilScreen.js
@@ -43,20 +43,20 @@ export default function ProfilScreen({ navigation }) {
     const updatedUserInfo = { token: user.token };
 
     // UPDATE USERNAME
-    if (username !== user.username) {
+    if (username !== user.username || username !== null || username!== "") {
       updatedUserInfo.username = username;
     } else {
       updatedUserInfo.username = user.username; // Use the old value
     }
     // UPDATE FIRSTNAME
-    if (firstname !== user.firstname) {
+    if (firstname !== user.firstname || firstname !== null || firstname!== "") {
       updatedUserInfo.firstname = firstname;
     } else {
       updatedUserInfo.firstname = user.firstname; // Use the old value
     }
 
     // UPDATE EMAIL
-    if (email !== user.email) {
+    if (email !== user.email ||email !== null || email!== "") {
       updatedUserInfo.email = email;
     } else {
       updatedUserInfo.email = user.email; // Use the old value
