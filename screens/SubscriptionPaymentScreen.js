fd0058e9e15ba15d7bdbb918fdc3b7c4f2fa8e79
Your name <Your email>
2023-10-27 17:38:39 +0200

    last commit day5

--- screens/SubscriptionPaymentScreen.js
+++ screens/SubscriptionPaymentScreen.js
@@ -7,11 +7,17 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function SubscriptionPaymentScreen({ navigation }) {
   const handleReturnToPayment = () => {
     // return to subscription page
-    navigation.navigate("SubscriptionPayment");
+    navigation.navigate("Subscription");
   };
+
   //   console.log("payment screen");
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.containerTitles}>
+        <Text style={styles.title1}>Paiement</Text>
+        <Text style={styles.title2}>Choisissez votre méthode de paiement</Text>
+        <Text style={styles.title3}>Vous serez débité une fois l'abonnement validé</Text>
+      </View>
       <View style={styles.arrowContainer}>
         <TouchableOpacity style={styles.arrowBtn}>
           <Image source={require("../assets/arrow-circle-back.png")} size={30} color={"#FFCE4A"} onPress={() => handleReturnToPayment()} />
@@ -22,12 +28,8 @@ export default function SubscriptionPaymentScreen({ navigation }) {
       </View>
       {/* <View style={styles.header}></View> */}
       <View style={styles.containerPayment}>
-        <View style={styles.containerTitles}>
-          <Text style={styles.title1}>Paiement</Text>
-          <Text style={styles.title2}>Choisissez votre méthode de paiement</Text>
-          <Text style={styles.title3}>Vous serez débité une fois l'abonnement validé</Text>
-        </View>
         <View style={styles.inputPayment}>
+          <Image source={require("../assets/buttonscanCard.png")} style={styles.scanCard} size={30} />
           <Text style={styles.titleInput}>Nom du propriétaire</Text>
           <TextInput style={styles.input}></TextInput>
           <Text style={styles.titleInput}>Numéro de carte</Text>
@@ -38,6 +40,7 @@ export default function SubscriptionPaymentScreen({ navigation }) {
             <Text style={styles.titleInput1}>Date d'expiration</Text>
             <TextInput style={styles.input1}>MM/AA</TextInput>
           </View>
+          <Text style={styles.title4}>sauvegarder mes informations pour la prochaine fois</Text>
         </View>
       </View>
     </SafeAreaView>
@@ -69,10 +72,16 @@ const styles = StyleSheet.create({
     fontFamily: "Lato_400Regular",
     borderColor: "red",
     borderWidth: 1,
+    fontSize: 16,
   },
   title3: {
     color: "white",
     fontFamily: "Lato_400Regular",
+    fontSize: 10,
+  },
+  scanCard: {
+    left: 130,
+    margin: 20,
   },
   titleInput: {
     color: "white",
@@ -103,6 +112,7 @@ const styles = StyleSheet.create({
     margin: 5,
     top: 30,
     right: 20,
+    color: "#FFCE4A",
   },
   arrowContainer: {
     position: "absolute",
@@ -122,5 +132,11 @@ const styles = StyleSheet.create({
   arrowBtn1: {
     bottom: 30,
     padding: 10,
+  },
+  title4: {
+    top: 70,
+    color: "white",
+    textAlign: "right",
+    marginRight: 20,
   },
 });
