1552c36c20cb75e14d320a57dafdf26e9809bcaa
Alienor <alienordms@gmail.com>
2023-10-29 20:01:56 +0100

    don

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -32,7 +32,7 @@ export default function HomeScreen({ navigation }) {
         <Text style={styles.title2}>Où les histoires</Text>
         <Text style={styles.title2bis}>prennent vie...</Text>
       </ImageBackground>
-
+    <View style={styles.btnContainer}>
       <TouchableOpacity style={styles.newStoryButton} onPress={() => handleSubmit()}>
           <Image
             style={styles.addButton}
@@ -45,16 +45,13 @@ export default function HomeScreen({ navigation }) {
       <Text style={styles.title3}>Votre dernière histoire</Text>
 
       <View style={styles.lastStoryButton}>
-        <View> 
-
-        </View>
         <ImageBackground style={styles.storyImage} source={require('../assets/ImageBibliotheque.png')} /*{{ uri: 'URL_DE_L'HISTOIRE' }}*/ >
           <Text style={styles.storyTitle}>TITRE DE L'HISTOIRE</Text>
         </ImageBackground>
-
         <TouchableOpacity style={styles.readButton} onPress={()=> handleDisplayStory()}>
           <Text style={styles.readButtonText}>Lire mon histoire</Text>
         </TouchableOpacity>
+      </View>
       </View>
        <TabBar navigation={navigation} />
 
@@ -74,7 +71,7 @@ const styles = StyleSheet.create({
   imagBgd: {
     flex: 2,
     width: "100%",
-    height: "45%",
+    height: "98%",
     marginTop:"-12%",// -30%
 
   },
@@ -105,24 +102,29 @@ const styles = StyleSheet.create({
     marginLeft: "3%",
   },
 
+  btnContainer:{
+    flex:3,
+    flexDirection:"column",
+    justifyContent:'flex-start',
+    alignItems:"center",
+    width: "100%",
+    marginTop:"15%"
+    
+  },
   newStoryButton: {
-    position: 'absolute',
-    top: '40%',
     width: '92%',
-    height: '17%',
+    height: '30%',
     padding: 15,
     borderWidth: 1,
     borderColor: '#FFCE4A',
     borderRadius: 10,
     alignItems: 'center',
-    justifyContent: 'flex-start',
     marginVertical: "5%",//20
   },
-
   addButton: {
     width: "9%",//30
-    height: "25%",//30
-    marginTop: "5%",//10
+    height: "28%",//30
+    marginTop: "3%",//10
     marginBottom: "4%",//10
   },
 
@@ -133,27 +135,23 @@ const styles = StyleSheet.create({
   },
 
   title3: {
-    position: 'absolute',
-    top: '60%',
     fontFamily: "Lato_400Regular",
     fontSize: 16,
     color: '#FFCE4A',
     alignSelf: 'flex-start',
     marginLeft: "4%",//16
-    marginTop: "12%",//20
+    marginTop: "4%",//20
   },
 
   lastStoryButton: {
-    position: 'absolute',
-    top: '68%',
     width: '92%',
-    height: '21%',
+    height: '40%',
     borderWidth: 1,
     borderColor: '#FFFFFF',
     borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'flex-start',
-    marginVertical: "5%",
+    marginVertical: "3%",
 },
 
   storyImage: {
