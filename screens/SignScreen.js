a3c5b7da62a50cde090e66e1d56ff7d47968b5d2
Jimmy Bellavia <bellavia.jimmy@gmail.com>
2023-11-02 14:47:13 +0100

    jim

--- screens/SignScreen.js
+++ screens/SignScreen.js
@@ -1,95 +1,125 @@
-import { StyleSheet, Text, TouchableOpacity, View, ImageBackground, Modal, TextInput} from "react-native";
+import { 
+  StyleSheet, 
+  Text, 
+  TouchableOpacity, 
+  View, 
+  ImageBackground, 
+  Modal, 
+  TextInput} from "react-native";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { updateUser } from "../reducers/user";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function SignScreen({ navigation }) {
+
+  // État pour la modal
   const [modalVisible, setModalVisible] = useState(false);
   const [modalType, setModalType] = useState("");
 
+  // États pour l'inscription
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [firstname, setFirstName] = useState("");
   const [email, setEmail] = useState("");
-  const [isValidSignIn, setIsValidSignIn] = useState(true);
   const [isValidSignUp, setIsValidSignUp] = useState(true);
-
+  
+  // État pour la connexion
   const [identifier, setIdentifier] = useState("");
   const [passwordUp, setPasswordUp] = useState("");
+  const [isValidSignIn, setIsValidSignIn] = useState(true);
 
-
+  // Accès au dispatch pour envoyer des actions.
   const dispatch = useDispatch();
   const user = useSelector(state => state.user.value);
 
+  // Visibilité de la modal + type de connexion (inscription/connexion)
   const handleModalToggle = type => {
     setModalType(type);
     setModalVisible(!modalVisible);
   };
 
+  // Inscription utilisateur
   const handleInscription = () => {
+    // Appel API pour créer l'utilisateur sur le serveur
     fetch("https://fable-forge-backend-84ce.vercel.app/users/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, firstname, email, password }),
     })
-      .then(response => response.json())
-      .then(data => {
-        if (data.result) {
-          setIsValidSignUp(true);
-          setUsername("");
-          setPassword("");
-          setFirstName("");
-          setEmail("");
-          dispatch(updateUser({ firstname, username, email, token: data.token }));
-          setModalVisible(false);
-          navigation.navigate("Home");
-        } else {
-          setIsValidSignUp(false);
-        }
-      });
-  };
+    .then(response => response.json())
+    .then(data => {
+      // En cas de succès, mise à jour de l'état et navigation
+      if (data.result) {
+        setIsValidSignUp(true);
+        resetSignUpForm();
+        dispatch(updateUser({ firstname, username, email, token: data.token }));
+        setModalVisible(false);
+        navigation.navigate("Home");
+      } else {
+        setIsValidSignUp(false);
+      }
+    });
+};
 
+// Réinitialiser le formulaire d'inscription après le succès
+const resetSignUpForm = () => {
+  setUsername("");
+  setPassword("");
+  setFirstName("");
+  setEmail("");
+};
+
+  // Connexion utilisateur
   const handleConnection = () => {
     fetch("https://fable-forge-backend-84ce.vercel.app/users/signin", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ identifier, password: passwordUp }),
     })
-      .then(response => response.json())
-      .then(data => {
-        if (data.result) {
-          setIsValidSignIn(true);
-          setIdentifier("");
-          setPasswordUp("");
-          dispatch(
-            updateUser({
-              firstname: data.firstname,
-              username: data.username,
-              email: data.email,
-              token: data.token,
-            })
-          );
-          setModalVisible(false);
-          navigation.navigate("Home");
-        } else {
-          setIsValidSignIn(false);
-        }
-      });
-  };
+    .then(response => response.json())
+    .then(data => {
+      // En cas de succès, mise à jour de l'état et navigation
+      if (data.result) {
+        setIsValidSignIn(true);
+        resetSignInForm();
+        dispatch(
+          updateUser({
+            firstname: data.firstname,
+            username: data.username,
+            email: data.email,
+            token: data.token,
+          })
+        );
+        setModalVisible(false);
+        navigation.navigate("Home");
+      } else {
+        setIsValidSignIn(false);
+      }
+    });
+};
 
+// Réinitialiser le formulaire de connexion après le succès
+const resetSignInForm = () => {
+  setIdentifier("");
+  setPasswordUp("");
+};
+
+  // Styles conditionnels en fonction du type de modale
   const modalStyle = modalType === "register" ? styles.modalContainerInscription : styles.modalContainerConnection;
   const titleStyle = modalType === "register" ? styles.titleModalUp : styles.titleModalIn;
   const closeStyle = modalType === "register" ? styles.closeModalUp : styles.closeModalIn;
 
+  // Texte d'erreur conditionnel pour l'inscription et la connexion
   const errorTextInStyle = isValidSignIn
-    ? styles.errorUpText // Style when isValidSignIn is true
-    : { ...styles.errorUpText, color: "#FFCE4A" }; // Style when isValidSignIn is false (you can change the color)
+    ? styles.errorUpText // Style quand isValidSignIn est vrai
+    : { ...styles.errorUpText, color: "#FFCE4A" }; // Style quand isValidSignIn est vrai
 
   const errorTextUpStyle = isValidSignUp
-    ? styles.errorInText // Style when isValidSignIn is true
-    : { ...styles.errorInText, color: "#FFCE4A" }; // Style when isValidSignIn is false (you can change the color)
+    ? styles.errorInText // Style quand isValidSignIn est vrai
+    : { ...styles.errorInText, color: "#FFCE4A" }; // Style quand isValidSignIn est vrai
 
+  // Vérifier si l'utilisateur est déjà connecté
   useEffect(() => {
     if (user.email != null || user.username != null) {
       navigation.navigate("Home");
@@ -192,26 +222,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#2C1A51",
   },
-  mdlctn: {
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(0,0,0, 0.5)",
-  },
-  containerBis: {
-    flex: 1,
-    width: "100%",
-  },
+
+// Style header
   imagBgd: {
     flex: 2,
     width: "100%",
     height: "90%",
-    // backgroundColor:
   },
   title1: {
     fontFamily: "Lato_400Regular",
     fontSize: 28,
-    // margin: 170,
-
     textAlign: "center",
     color: "#FFFFFF",
     marginTop: "135%",
@@ -221,8 +241,10 @@ const styles = StyleSheet.create({
     fontSize: 56,
     textAlign: "center",
     color: "#FFFFFF",
-    lineHeight: 60,//60
+    lineHeight: 60,
   },
+
+// Style bouton connexion/inscription
   btnHome: {
     alignItems: "center",
     backgroundColor: "#FFCE4A",
@@ -238,7 +260,7 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent",
     borderRadius: 10,
     margin: "2%",
-    padding: "2%",//10
+    padding: "2%",
     width: "80%",
     height: "5%",
     justifyContent: "center",
@@ -256,31 +278,35 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#FFFFFF",
   },
+
+// Style modal
+  mdlctn: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0,0,0, 0.5)",
+  },
   modalContainerInscription: {
-    width: "86%", // Adjust the width as per your requirement 350
-    height: 400, // Adjust the height as per your requirement 400 // 47
-    marginTop: 130, // 200
-    marginLeft: "7%", //30
+    width: "86%",
+    height: 400,
+    marginTop: 130,
+    marginLeft: "7%",
     backgroundColor: "#6B5F85",
-    borderRadius: 20, // Adjust the borderRadius as per your requirement
+    borderRadius: 20,
     padding: '4%',
     alignItems: "center",
     justifyContent: "center",
-    // backdropFilter: "blur(5px)",
   },
   modalContainerConnection: {
-    width: "86%", // Adjust the width as per your requirement
-    height: 290, // Adjust the height as per your requirement 300
+    width: "86%",
+    height: 290,
     marginTop: "63%",
     marginLeft: "7%",
     backgroundColor: "#6B5F85",
-    borderRadius: 20, // Adjust the borderRadius as per your requirement
+    borderRadius: 20,
     padding: '4%',
     alignItems: "center",
     justifyContent: "center",
-    // backdropFilter: "blur(5px)",
   },
-
   imagBgdModal: {
     height: "100%",
     width: "100%",
@@ -292,13 +318,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-
   titleModalIn: {
     fontFamily: "Lato_400Regular",
     color: "white",
-    fontSize: 18, // 18
+    fontSize: 18,
     padding: "2%",
-    marginTop: "-8%", // -40
+    marginTop: "-8%",
   },
   titleModalUp: {
     fontFamily: "Lato_400Regular",
@@ -318,41 +343,45 @@ const styles = StyleSheet.create({
     top: "-2%",
     left: "62%",
   },
+
+// Style input et bouton modal
   inputIn: {
     width: "90%",
-    height: "15%", //40
+    height: "15%",
     borderColor: "white",
     color: "white",
     fontFamily: "Lato_400Regular",
     borderWidth: 1,
-    marginBottom: "5%", // 15
+    marginBottom: "5%",
     paddingLeft: 10,
     borderRadius: 10,
   },
   inputUp: {
     width: "90%",
-    height: "11%", //40
+    height: "11%",
     borderColor: "white",
     color: "white",
     fontFamily: "Lato_400Regular",
     borderWidth: 1,
-    marginBottom: "5%", // 15
+    marginBottom: "5%",
     paddingLeft: 10,
     borderRadius: 10,
   },
   btnValidate: {
     backgroundColor: "#FFCE4A",
     borderRadius: 10,
-    margin: 10, // 10
-    padding: 10, // 10
+    margin: 10,
+    padding: 10,
     textAlign: "center",
     width: "90%",
-    marginBottom: 10, // 10
+    marginBottom: 10,
   },
   textBtnValidate: {
     fontFamily: "Lato_400Regular",
     textAlign: "center",
   },
+
+  // Style pour les messages d'erreur
   errorInText: {
     color: "#6B5F85",
     marginTop: 5,
